33f3cf916106f7b9e100add5aaa6cf50156a54f7
javahometech <kammana.hari@gmail.com>
2016-09-29 19:13:48 +0530

    Update janardhan.java

--- janardhan.java
+++ janardhan.java
@@ -2,6 +2,8 @@ public class Cricket
 {
 public static void main( String args[] )
 { 
-System.out.println("Hello Java")
+System.out.println("Hello Java");
+System.out.println("------")
+System.out.println("Bye Java");
 }
 }
